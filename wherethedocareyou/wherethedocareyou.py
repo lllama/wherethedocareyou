04c514f8e8d5bf0281aed25f4e54cf05958c8711
Kristian Glass <git@doismellburning.co.uk>
2013-05-25 20:29:59 +0100

    MongoDB data. Can you tell I'm at a "hackday"?

--- wherethedocareyou/wherethedocareyou.py
+++ wherethedocareyou/wherethedocareyou.py
@@ -1,16 +1,26 @@
 # -*- coding: UTF-8 -*-
 
 from __future__ import print_function, division, absolute_import, unicode_literals
+from urlparse import urlparse
 
 import flask
 import os
+import pymongo
 
 app = flask.Flask(__name__)
 
 x = 500
 y = 430
 
+MONGO_URL = os.environ['MONGOHQ_URL']
+mongo_client = pymongo.MongoClient(MONGO_URL)
+db = mongo_client[urlparse(MONGO_URL).path[1:]]
+
 @app.route("/whereami")
+def whereami():
+    return coordinates()
+
+@app.route("/location/me")
 def coordinates():
     # Sick hackery...
     import random
@@ -29,7 +39,16 @@ def map():
 
 @app.route("/badger", methods=['POST'])
 def badger():
+    # Because why would I name the function sensibly or provide useful output?
     print(str(flask.request.json))
+    map_data = db['map_data']
+    map_data.insert(flask.request.json)
+    return ""
+
+@app.route("/location/me/update", methods=['POST'])
+def update_location():
+    locations = db['locations']
+    locations.insert(flask.request.json)
     return ""
 
 if __name__ == "__main__":
