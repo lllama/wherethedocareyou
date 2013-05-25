fa94acefdb337cabe63966d2a51ffbfd20d91c63
Kristian Glass <git@doismellburning.co.uk>
2013-05-25 13:41:45 +0100

    Skeleton

---
+++ wheresupdoc/wheresupdoc.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+import flask
+import os
+
+app = flask.Flask(__name__)
+
+@app.route("/whereami")
+def coordinates():
+    return flask.jsonify({'x': 0, 'y': 0})
+
+@app.route("/")
+def home():
+    return ""
+
+if __name__ == "__main__":
+    PORT = os.getenv('PORT', 5000)
+    app.debug = True
+    app.run(port=PORT)
